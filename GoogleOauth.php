67340b7e42a6da776f7b6882c9eab45b302b6b2d
Duncan Barnes <github@duncanbarnes.net>
2023-07-05 22:42:44 +0100

    Added a prefetch for the hover image
    This prevents the slight flash caused by the hover image being loaded by using HTML5 prefetch. Seems preferable over single image with offsets as that would require modifying Google's images which are supplied as separate files now, and in the future if they change.

--- GoogleOauth.php
+++ GoogleOauth.php
@@ -75,6 +75,7 @@ class GoogleOauthPlugin extends MantisPlugin {
 				}
 			</style>
 			<script type="text/javascript" src="'.plugin_file("plugin.js").'"></script>
+			<link rel="prefetch" href="'.plugin_file("google_signin_pressed.png").'" />
 		';
 	}
 }
